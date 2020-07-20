72c226ed97c42fc8970fbd1b11be82ae27da63dc
rajeshAmbi <65827760+rajeshAmbi@users.noreply.github.com>
2020-07-20 12:12:16 -0400

    Update OnlineTest.java

--- onlineTest/OnlineTest.java
+++ onlineTest/OnlineTest.java
@@ -10,8 +10,7 @@ class OnlineTest extends JFrame implements ActionListener
 	JRadioButton jb[]=new JRadioButton[5];
 	JButton b1,b2;
 	ButtonGroup bg;
-	int count=0,current=0,x=1,y=1,now=0,rajesh=1;
-	string r1="rajesh";
+	int count=0,current=0,x=1,y=1,now=0;
 	int m[]=new int[10];	
 	OnlineTest(String s)
 	{
